b849f43ca724bbef64872ac2082c7ef7aa384b74
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-05-15 11:59:26 -0300

    feat(src): create "index.ts".
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { getInput, setFailed } from '@actions/core'
+import * as param from '@actions/github'
+import { validateParamAtPullRequest } from './service'
+
+const gitHubToken = getInput('gitHubToken')
+
+async function run(): Promise<void> {
+  try {
+    if (gitHubToken) {
+      const conditionalValue = getInput('conditionalValue')
+      const valueThatMustExist = getInput('valueThatMustExist')
+      const pullRequestNumber = Number(param.context.ref.split('/')[2])
+      const repoName = param.context.payload.repository?.owner.login
+      const repoOwner = param.context.payload.repository?.name
+
+      await validateParamAtPullRequest(
+        conditionalValue,
+        pullRequestNumber,
+        repoName as string,
+        repoOwner as string,
+        valueThatMustExist,
+      )
+    } else {
+      setFailed(`"gitHubToken" is required!`)
+    }
+  } catch (error) {
+    setFailed(`Error at action: ${error}.`)
+  }
+}
+
+void run()
