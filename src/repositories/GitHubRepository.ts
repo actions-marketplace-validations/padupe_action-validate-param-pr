455a5b14a3be82371e4fde9324f6805e3f218d90
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-05-15 11:55:53 -0300

    feat(src/repositories): create "GitHubRepository" class.
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

---
+++ src/repositories/GitHubRepository.ts
@@ -0,0 +1,56 @@
+import { Octokit } from '@octokit/core'
+import { IGitHubRepository } from './IGitHubRepository'
+import { auth } from 'auth'
+import { setFailed } from '@actions/core'
+
+export class GitHubRepository implements IGitHubRepository {
+  private readonly repository: Octokit
+
+  constructor() {
+    this.repository = auth
+  }
+
+  async createCommentAtPR(
+    message: string,
+    pullRequestNumber: number,
+    repoName: string,
+    repoOwner: string,
+  ): Promise<object> {
+    const comment = await this.repository.request(
+      `POST /repos/{owner}/{repo}/issues/{issue_number}/comments`,
+      {
+        owner: repoOwner,
+        repo: repoName,
+        issue_number: pullRequestNumber,
+        body: message,
+      },
+    )
+
+    if (comment.status !== 201) {
+      setFailed(`Error creating comment ou Pull Request ${pullRequestNumber}.`)
+    }
+
+    return comment.data
+  }
+
+  async getPullRequestNumber(
+    pullRequestNUmber: number,
+    repoName: string,
+    repoOwner: string,
+  ): Promise<string> {
+    const pullRequest = await this.repository.request(
+      `GET /repos/{owner}/{repo}/pulls/{pull_number}`,
+      {
+        owner: repoOwner,
+        repo: repoName,
+        pull_number: pullRequestNUmber,
+      },
+    )
+
+    if (!pullRequest) {
+      setFailed(`Error capturing Pull Request body ${pullRequestNUmber}`)
+    }
+
+    return pullRequest.data.body as string
+  }
+}
