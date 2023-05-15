5aed0cc68e41ec7f807d5c3ddea242f2b1681493
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-05-15 11:52:44 -0300

    feat(src/service): create "gitHubService.ts" function.
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

---
+++ src/service/gitHubService.ts
@@ -0,0 +1,42 @@
+import { setFailed, setOutput } from '@actions/core'
+import { validationIfConditionIsMet } from '@utils/validationIfConditionIsMet'
+import { GitHubRepository } from 'repositories'
+
+const gitHubRepository = new GitHubRepository()
+
+export async function gitHubService(
+  conditionalValue: string,
+  pullRequestNumber: number,
+  repoName: string,
+  repoOwner: string,
+  valueThatMustExist: string,
+): Promise<any> {
+  const bodyPullRequest = await gitHubRepository.getPullRequestNumber(
+    pullRequestNumber,
+    repoName,
+    repoOwner,
+  )
+
+  const validate = validationIfConditionIsMet(
+    bodyPullRequest,
+    conditionalValue,
+    valueThatMustExist,
+  )
+
+  if (validate === false) {
+    await gitHubRepository.createCommentAtPR(
+      `The value ${valueThatMustExist} does not exist, as expected when we have ${conditionalValue}.`,
+      pullRequestNumber,
+      repoName,
+      repoOwner,
+    )
+
+    setFailed(
+      `The value ${valueThatMustExist} does not exist, as expected when we have ${conditionalValue}.`,
+    )
+
+    return setOutput('result', false)
+  }
+
+  return setOutput('result', true)
+}
